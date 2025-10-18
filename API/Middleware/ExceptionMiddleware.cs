52e230fdb33e7445105dca349b8ed5c90112d013
Hilda <hildajafari01@gmail.com>
2025-10-18 11:37:02 +0330

    End of section 6

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,47 @@
+using System.Net;
+using System.Text.Json;
+using Microsoft.Extensions.Hosting;
+using API.Errors; 
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionMiddleware(RequestDelegate next, IHostEnvironment env)
+        {
+            _next = next;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex, _env);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex, IHostEnvironment env)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+           
+            var response = env.IsDevelopment()
+                ? new ApiErrorResponse(context.Response.StatusCode, ex.Message, ex.StackTrace)
+                : new ApiErrorResponse(context.Response.StatusCode, "Internal Server Error");
+
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            var json = JsonSerializer.Serialize(response, options);
+
+            return context.Response.WriteAsync(json);
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -14,10 +15,17 @@ builder.Services.AddDbContext<StoreContext>(options =>
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddCors();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+   .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 
 app.MapControllers();
 
