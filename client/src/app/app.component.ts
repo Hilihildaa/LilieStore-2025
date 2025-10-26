c6dd798e1018ddccaa9fc00a552429fa1841dbce
Hilda <hildajafari01@gmail.com>
2025-10-26 09:33:13 +0330

    End of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './core/shared/models/product';
+import { Pagination } from './core/shared/models/pagination';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
   title = 'LiliStore';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => {
+        this.products = response.data;
+        console.log('Products:', this.products);
+      },
+      error: error => console.log('Error:', error),
+      complete: () => console.log('Request complete')
+    });
+  }
 }
+
+
+//این کد، وقتی اپ شروع می‌شه، می‌ره سراغ سرور محلی و داده‌های محصولات رو می‌گیره و در کنسول چاپ می‌کنه.
---
+++ client/src/app/core/shared/models/product.ts
@@ -0,0 +1,9 @@
+export type Product = {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    type: string;
+    brand: string;
+    quantityInStock: number;
+};
